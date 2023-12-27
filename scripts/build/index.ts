3a7d0a8b357191750bb48665bc156e3df5c6e65c
nonepointer666 <1194203968@qq.com>
2023-12-28 01:13:48 +0900

    chore: gulp使用方式调整

--- scripts/build/index.ts
+++ scripts/build/index.ts
@@ -1,32 +1,30 @@
-import delPath from '../utils/delpath';
-import { series, parallel, src, dest } from 'gulp';
-import { pkgPath, componentPath } from '../utils/paths';
-import less from 'gulp-less';
-import autoprefixer from 'gulp-autoprefixer';
-import run from '../utils/run';
+import delPath from "../utils/delpath";
+import { series, parallel, src, dest } from "gulp";
+import { pkgPath, componentPath, rootPath } from "../utils/paths";
+import less from "gulp-less";
+import autoprefixer from "gulp-autoprefixer";
+import run from "../utils/run";
 //删除dist
-
-export const removeDist = () => {
-  return delPath(`${pkgPath}/easyest`);
+const removeDist = () => {
+  return delPath(`${rootPath}/dist/fiery/vue`);
 };
 
-//打包样式
-export const buildStyle = () => {
+const buildStyle = () => {
   return src(`${componentPath}/src/**/style/**.less`)
     .pipe(less())
     .pipe(autoprefixer())
-    .pipe(dest(`${pkgPath}/easyest/lib/src`))
-    .pipe(dest(`${pkgPath}/easyest/es/src`));
+    .pipe(dest(`${rootPath}/dist/fiery/vue/lib/src`))
+    .pipe(dest(`${pkgPath}/dist/fiery/vue/es/src`));
 };
 
-//打包组件
-export const buildComponent = async () => {
-  run('pnpm run build', componentPath);
+const buildComponent = async () => {
+  run("pnpm run build", componentPath);
 };
+
 export default series(
   async () => removeDist(),
   parallel(
+    async () => buildComponent(),
     async () => buildStyle(),
-    async () => buildComponent()
-  )
+  ),
 );
